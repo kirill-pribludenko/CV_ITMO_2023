20ed2a3d39d6a4c44940a07f99b10d8e28309f8f
Kirill <gipokrat089@gmail.com>
2023-03-08 22:23:12 +0300

    Update processing_after_label.py
    add func - read rle mask and draw it on template mask

--- data/processing_after_label.py
+++ data/processing_after_label.py
@@ -10,7 +10,23 @@ from rasterio.merge import merge
 LABEL_FILE_NAME = "./data/output/annotations.xml"
 INPUT_MASKS_DIR = "./data/output/for_classic_way/mask_template/"
 OUTPUT_PATH = "./data/output/for_classic_way/mask/"
-OUTPUT_FILE = "./data/output/for_torchgeo_way/mask.tif"
+OUTPUT_FILE = "./data/output/for_torchgeo_way/mask/mask.tif"
+
+
+def rle_decode(input_list, size_of_mask):
+    new_str = ""
+    new_size = [size + 1 for size in size_of_mask]
+
+    for i, number in enumerate(input_list):
+        condition = i % 2
+        if condition == 0:
+            new_str += "0 " * number
+        elif condition == 1:
+            new_str += "255 " * number
+
+    new_array = np.fromstring(new_str, dtype=int, sep=" ")
+
+    return new_array.reshape(new_size)
 
 
 def main():
@@ -23,6 +39,7 @@ def main():
     # {'name_of_file_N': False
     # } - if polygons not exist
 
+    print("Start reading XML file....")
     tree = ET.parse(LABEL_FILE_NAME)
     root = tree.getroot()
     dict_masks = {}
@@ -36,15 +53,41 @@ def main():
         if len(root[i]) > 0:
             dict_temp = {}
             for j in range(len(root[i])):
-                text_coords = root[i][j].attrib["points"].replace(";", ",")
-                array_points = np.array(text_coords.split(",")).astype(float)
-                array_points = array_points.reshape(-1, 2).astype(int)
-                dict_temp[f"polygon_{j}"] = array_points
+                tag = root[i][j].tag
+                if tag == "polygon":
+                    # parse data from polygon
+                    text_coords = root[i][j].attrib["points"].replace(";", ",")
+                    array_points = np.array(text_coords.split(",")).astype(float)
+                    array_points = array_points.reshape(-1, 2).astype(int)
+                    dict_temp[f"polygon_{j}"] = array_points
+                elif tag == "mask":
+                    # parse data from mask
+                    text_coords = root[i][j].attrib["rle"]
+                    # top - stroka, left - colonka
+                    # height - stroka, width - colonka
+                    left_top_point = [
+                        int(root[i][j].attrib["top"]),
+                        int(root[i][j].attrib["left"]),
+                    ]
+                    size_of_mask = [
+                        int(root[i][j].attrib["height"]),
+                        int(root[i][j].attrib["width"]),
+                    ]
+                    array_np = np.fromstring(text_coords, dtype=int, sep=",").tolist()
+                    decoded_array = rle_decode(array_np, size_of_mask)
+                    dict_temp[f"mask_{j}"] = {
+                        "array": decoded_array,
+                        "left_top_point": left_top_point,
+                    }
+                else:
+                    print("Error of type annotation")
+
             dict_masks[correct_name] = dict_temp
         else:
             dict_masks[correct_name] = False
 
     # Fill all polygons in templates masks ------------------------------------
+    print("Start creating masks....")
     for file_name in dict_masks.keys():
         # open file and transform it to array
         mask_orig = rio.open(INPUT_MASKS_DIR + file_name)
@@ -65,10 +108,24 @@ def main():
                 dst.write(img_array, indexes=1)
         # for case when label exist, fill all polygons and save file
         else:
-            for polygon_array in dict_masks[file_name].values():
-                mask = cv2.fillPoly(
-                    img_array, pts=[polygon_array], color=(255, 255, 255)
-                )
+            final_mask = np.zeros(img_array.shape)
+            mask_m = np.zeros(img_array.shape)
+            for i, key in enumerate(dict_masks[file_name].keys()):
+                if key[:1] == "p":
+                    polygon_array = dict_masks[file_name][key]
+                    mask_p = cv2.fillPoly(img_array, pts=[polygon_array], color=(255))
+                    final_mask += mask_p
+
+                elif key[:1] == "m":
+                    mask_array = dict_masks[file_name][key]["array"]
+                    left_top_point = dict_masks[file_name][key]["left_top_point"]
+                    from_row = left_top_point[0]
+                    to_row = left_top_point[0] + mask_array.shape[0]
+                    from_col = left_top_point[1]
+                    to_col = left_top_point[1] + mask_array.shape[1]
+                    mask_m[from_row:to_row, from_col:to_col] = mask_array
+                    final_mask += mask_m
+
             with rio.open(
                 (OUTPUT_PATH + file_name),
                 "w",
@@ -79,11 +136,10 @@ def main():
                 height=mask_orig.height,
                 transform=mask_orig.transform,
             ) as dst:
-                dst.write(mask, indexes=1)
-        # closing file
-        mask_orig.close()
+                dst.write(final_mask, indexes=1)
 
     # Merge all mask files to one big mask ----------------------------------
+    print("Start merge....")
     img_paths = glob.glob(os.path.join(OUTPUT_PATH, "*.tif"))
     img_paths.sort()
     tepmlate_to_full_mask = []
