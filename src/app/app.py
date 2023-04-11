af9ffae2ed47d61dce35b741b159c7e7180fff50
Kirill <gipokrat089@gmail.com>
2023-04-12 00:16:24 +0300

    Change app.py

--- src/app/app.py
+++ src/app/app.py
@@ -1,71 +1,107 @@
-import asyncio
+import io
 import os
-import shutil
-import subprocess
+import warnings
+
+import numpy as np
+import onnxruntime as ort
+import rasterio as rio
+import torch
+import uvicorn
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
+from PIL import Image
+from torchgeo.transforms import indices
+from torchvision.utils import draw_segmentation_masks
+
+warnings.filterwarnings("ignore", category=rio.errors.NotGeoreferencedWarning)
 
 app = FastAPI()
 
+# Load the ONNX model
+sess = ort.InferenceSession("./models/test_torchgeo.onnx")
 
-# Функция для выполнения предсказаний
+# Define the HTML template for the main page
+html_template = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Heracleum Segmentation</title>
+    </head>
+    <body>
+        <h1>Heracleum Segmentation</h1>
+        <form action="/predict/" method="post" enctype="multipart/form-data">
+            <input type="file" name="file">
+            <button type="submit">Predict</button>
+        </form>
+    </body>
+</html>
+"""
 
 
+# Define the endpoint to serve the main page
 @app.get("/")
-async def serve_index():
-    return FileResponse("./src/app/ing.html")
+async def main():
+    return HTMLResponse(content=html_template, status_code=200)
 
 
-# Путь до директории, где будут сохраняться изображения
-STATIC_FILES_PATH = "./models/inference/example_imgs"
+tfms = torch.nn.Sequential(indices.AppendNDVI(index_nir=3, index_red=0))
 
 
+# Define the endpoint to handle file uploads
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
+    # Read the image file from the request
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
 
-    # Удаляем все файлы в указанной папке
-    for filename in os.listdir(STATIC_FILES_PATH):
-        file_path = os.path.join(STATIC_FILES_PATH, filename)
-        try:
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-        except Exception as e:
-            print(e)
+    # Prepare image
+    img_a = np.array(image).astype(np.float32)
+    # from 0 - 255 to 0.0 - 1.0
+    img_a /= 255.0
+    img_t = torch.from_numpy(img_a.transpose((2, 0, 1)))
+    # add 1 dimension
+    img_t = img_t[None, :, :, :]
+    img_t = tfms(img_t)
+    img_f = img_t.numpy()
 
-    # сохраняем загруженный файл в указанной папке
-    file_location = os.path.join(STATIC_FILES_PATH, file.filename)
-    with open(file_location, "wb+") as file_object:
-        shutil.copyfileobj(file.file, file_object)
+    # Run the image through the model
+    input_name = sess.get_inputs()[0].name
+    output_name = sess.get_outputs()[0].name
+    pred = sess.run([output_name], {input_name: img_f})[0]
 
-    # запускаем скрипт с предсказаниями в отдельном процессе
-    cmd = (
-        "python ./src/models/predict_torchgeo.py --weights ./models/test_torchgeo.pt "
-        f"--source {STATIC_FILES_PATH} --img-size 256"
+    # Convert the predicted result to an image
+    pred_t = torch.from_numpy(pred)
+    normalized_masks = torch.nn.functional.softmax(pred_t, dim=1)
+    class_to_idx = {"no_heracleum": 0, "heracleum": 1}
+    boolean_masks = normalized_masks.argmax(1) == class_to_idx["heracleum"]
+
+    img_t = (255 * img_t[0, :3]).type(torch.uint8)
+    img_and_pred = draw_segmentation_masks(
+        img_t, masks=boolean_masks[0], alpha=0.6, colors="red"
     )
 
-    subprocess.Popen(cmd.split())
-
-    # Возвращаем исходное изображение и его результаты
-    img_path = os.path.join(STATIC_FILES_PATH, file.filename)
-    predictions_path = os.path.join(
-        STATIC_FILES_PATH, f"predictions_{os.path.splitext(file.filename)[0]}.jpg"
+    # Save the predicted image to a temporary file
+    temp_file = rio.open(
+        "./models/inference/temp_imgs/predict.png",
+        "w",
+        width=256,
+        height=256,
+        count=3,
+        dtype=np.uint8,
     )
-    while not os.path.exists(predictions_path):
-        await asyncio.sleep(1)
+    temp_file.write(img_and_pred)
+    temp_file.close()
 
-    return {
-        "original_image": FileResponse(img_path),
-        "predictions": FileResponse(predictions_path),
-    }
-
-
-@app.get("/download/")
-async def download_file():
-    folder_path = "./models/inference/results/"
+    # Find last file
+    folder_path = "./models/inference/temp_imgs/"
     folder_files = os.listdir(folder_path)
-    folder_files = [
-        os.path.join(folder_path, file) for file in folder_files
-    ]
+    folder_files = [os.path.join(folder_path, file) for file in folder_files]
     folder_files.sort(key=os.path.getctime)
     latest_file = folder_files[-1]
-    return FileResponse(latest_file)
+
+    # Return the predicted image as a file response
+    return FileResponse(latest_file, media_type="image/png")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
